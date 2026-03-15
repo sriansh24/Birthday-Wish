ba7108368854f96484c298d83bcbb604f19d6441
sriansh24 <pattanaiksrianshu@gmail.com>
2026-03-15 23:48:00 +0530

    Publishing Happybirthday website.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import BirthdayWish from "./page/BirthdayWish/BirthdayWish";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/birtday-wish/birtday-wish" />} />
+      <Route path="/birtday-wish/birtday-wish" element={<BirthdayWish />} />
+    </Routes>
+  );
+}
